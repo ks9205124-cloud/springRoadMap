f622b0e6d66bcd6a323e024dfe205e8f3995735b
Shaurya shukla <ks9205124@gmail.com>
2026-06-16 13:13:52 +0530

    ch01: setup home page controller, Thymeleaf template and unit test

---
+++ shaurya-cloud/src/main/java/com/shaurya/spring/shauryacloud/ShauryaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.spring.shauryacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShauryaCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShauryaCloudApplication.class, args);
+    }
+
+}
