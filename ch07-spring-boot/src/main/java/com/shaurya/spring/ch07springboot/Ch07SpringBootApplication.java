e5586d749a90962025f5ea539debe627ed4ace2f
Shaurya shukla <ks9205124@gmail.com>
2026-06-18 16:09:15 +0530

    feat(ch07): configure Maven parent POM and migrate to Spring Boot starters

---
+++ ch07-spring-boot/src/main/java/com/shaurya/spring/ch07springboot/Ch07SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.shaurya.spring.ch07springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch07SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch07SpringBootApplication.class, args);
+    }
+
+}
---
+++ ch07-spring-boot/src/test/java/com/shaurya/spring/ch07springboot/Ch07SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaurya.spring.ch07springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch07SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
